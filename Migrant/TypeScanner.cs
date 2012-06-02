c062549c935ac3c0f2a6bf41ab9c444fd6a1e64f
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2012-06-02 15:25:07 +0200

    TypeScanner: Do not report abstract types.
    Not only interfaces, but abstract classes should not be reported
    as well. This is especially important in case of generated
    serializer, when we have less generation and get rid of quirks
    like the abstract Array type.

--- Migrant/TypeScanner.cs
+++ Migrant/TypeScanner.cs
@@ -93,7 +93,7 @@ namespace AntMicro.Migrant
             {
                 ScanRecursiveWithStack(typeSet, type, typeStack);
             }
-            if(!IsSerializable(typeToScan) || !typeSet.Add(typeToScan))
+            if(!IsImplementation(typeToScan) || !typeSet.Add(typeToScan))
             {
                 return;
             }
@@ -106,7 +106,7 @@ namespace AntMicro.Migrant
                 return;
             }
             var fields = typeToScan.GetAllFields(false).Where(Helpers.IsNotTransient);
-            var typesToAdd = fields.Select(x => x.FieldType).Where(x => !x.IsInterface)
+            var typesToAdd = fields.Select(x => x.FieldType).Where(x => IsImplementation(x))
                 .Distinct();
             foreach(var type in typesToAdd)
             {
@@ -138,9 +138,9 @@ namespace AntMicro.Migrant
             return type.IsPrimitive || type.IsEnum;
         }
 
-        private static bool IsSerializable(Type type)
+        private static bool IsImplementation(Type type)
         {
-            return !type.IsInterface; //no double checks
+            return !type.IsInterface && !type.IsAbstract;
         }
 
         private static bool ShouldFieldsBeScanned(Type type)
