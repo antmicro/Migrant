antmicro/Migrant

commit 6baa7e85c42ed35271925ee15d8c4f684b2250be

Invoke hook methods using delegates not reflection.

Migrant/Helpers.cs
@@ -205,7 +205,8 @@ namespace AntMicro.Migrant
 			var methodsToInvoke = GetMethodsWithAttribute(attributeType, o.GetType());
 			foreach(var method in methodsToInvoke)
 			{
-				method.Invoke(o, new object[0]);
+				var methodToInvoke = (Action)Delegate.CreateDelegate(typeof(Action), o, method);
+				methodToInvoke();
 			}
 		}
 

commit 81fe0526f31c85aeef7d15a581f1a16425b81e95

Fixed bug introduced by 6baa7e85c42ed35271925ee15d8c4f684b2250be.
We shouldn't give the first parameter if the method is static.

Migrant/Helpers.cs
@@ -205,7 +205,7 @@ namespace AntMicro.Migrant
 			var methodsToInvoke = GetMethodsWithAttribute(attributeType, o.GetType());
 			foreach(var method in methodsToInvoke)
 			{
-				var methodToInvoke = (Action)Delegate.CreateDelegate(typeof(Action), o, method);
+				var methodToInvoke = (Action)Delegate.CreateDelegate(typeof(Action), method.IsStatic ? null : o, method);
 				methodToInvoke();
 			}
 		}