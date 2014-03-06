ccf0f2b6107265d6798e630631562b4c97152615
Mateusz Hołenko <mholenko@antmicro.com>
2014-03-06 16:28:41 +0100

    Version bump (closes #68).

--- Migrant/Serializer.cs
+++ Migrant/Serializer.cs
@@ -217,7 +217,7 @@ namespace Antmicro.Migrant
 		private readonly Dictionary<Type, DynamicMethod> readMethodCache;
         private readonly InheritanceAwareList<Delegate> surrogatesForObjects;
         private readonly InheritanceAwareList<Delegate> objectsForSurrogates;
-		private const byte VersionNumber = 1;
+		private const byte VersionNumber = 2;
 		private const byte Magic1 = 0x32;
 		private const byte Magic2 = 0x66;
 		private const byte Magic3 = 0x34;
