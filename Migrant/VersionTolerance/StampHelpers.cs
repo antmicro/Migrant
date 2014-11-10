4585d5eb6e3f0373ec759a3c8b4825ba23fd28d9
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2014-11-10 10:43:44 +0100

    Removed unused debug functions.

--- Migrant/VersionTolerance/StampHelpers.cs
+++ Migrant/VersionTolerance/StampHelpers.cs
@@ -27,7 +27,6 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
-using System.Text;
 
 namespace Antmicro.Migrant.VersionTolerance
 {
@@ -46,63 +45,6 @@ namespace Antmicro.Migrant.VersionTolerance
         public static IEnumerable<Tuple<Type, IEnumerable<FieldInfo>>> GetFieldsStructureInSerializationOrder(Type type, bool withTransient = false)
         {
             return type.GetAllFieldsStructurized().Select(x => Tuple.Create(x.Item1, x.Item2.Where(y => withTransient || Helpers.IsNotTransient(y)).OrderBy(y => y.Name).AsEnumerable()));
-        }
-
-        public static void ToPrettyString(this TypeStamp.TypeStampCompareResult results)
-        {
-            Console.WriteLine("Fields added:");
-            foreach(var f in results.FieldsAdded)
-            {
-                Console.WriteLine("\t" + f.Name);
-            }
-            Console.WriteLine("Fields removed:");
-            foreach(var f in results.FieldsRemoved)
-            {
-                Console.WriteLine("\t" + f.Name);
-            }
-            Console.WriteLine("Fields moved:");
-            foreach(var f in results.FieldsMoved)
-            {
-                Console.WriteLine("\t" + f.Key.Name + " " + f.Key.OwningTypeAQN + " => " + f.Value.OwningTypeAQN);
-            }
-            Console.WriteLine("Fields changed:");
-            foreach(var f in results.FieldsChanged)
-            {
-                Console.WriteLine("\t" + f.Name);
-            }
-        }
-
-        public static string ToPrettyString(this TypeDescriptor type)
-        {
-            var bldr = new StringBuilder();
-            bldr.AppendFormat("Type name: {0}\n", type.AssemblyQualifiedName);
-            foreach(var f in type.Fields)
-            {
-                bldr.Append(f);
-            }
-            return bldr.ToString();
-        }
-
-        public static string ToPrettyString(this TypeStamp stamp)
-        {
-            var bldr = new StringBuilder();
-            bldr.AppendLine("-----------------");
-            bldr.AppendFormat("GUID: {0}\n", stamp.ModuleGUID);
-            foreach(var c in stamp.Classes)
-            {
-                bldr.Append(c).AppendLine();
-            }
-            bldr.AppendLine("-----------------");
-            return bldr.ToString();
-        }
-
-        public static string ToPrettyString(this FieldDescriptor field)
-        { 
-            var bldr = new StringBuilder();
-            bldr.AppendFormat("Name: {0}\n", field.Name);
-            bldr.AppendFormat("Type: {0}\n", field.TypeAQN);
-            bldr.AppendFormat("Owned by: {0}\n", field.OwningTypeAQN);
-            return bldr.ToString();
         }
     }
 }
