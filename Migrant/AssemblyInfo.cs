a29f823673bec0fc4b49913db4d20567992cdb9c
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2014-12-16 22:12:12 +0100

    Version bump.

--- Migrant/AssemblyInfo.cs
+++ Migrant/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Antmicro")]
 [assembly: AssemblyCopyright("Copyright by Antmicro 2012 - 2014")]
 
-[assembly: AssemblyVersion("0.7.3")]
+[assembly: AssemblyVersion("0.7.4")]
 
 [assembly: InternalsVisibleTo("Tests")]
