2b35fb8ef7e066002ad4852cf8c416ef685d5031
Mateusz Hołenko <mholenko@antmicro.com>
2015-04-30 12:14:40 +0200

    Removal of unused overload method together with helper data structure.

--- Migrant/Helpers.cs
+++ Migrant/Helpers.cs
@@ -203,11 +203,6 @@ namespace Antmicro.Migrant
             return TypesWriteableByPrimitiveWriter.Contains(type);
         }
 
-        public static bool IsWriteableByPrimitiveWriter(TypeDescriptor type)
-        {
-            return TypesWriteableByPrimitiveWriterAQNs.Contains(type.AssemblyQualifiedName);
-        }
-
         internal static bool CheckTransientNoCache(Type type)
         {
             return type.IsDefined(typeof(TransientAttribute), true);
@@ -259,14 +254,10 @@ namespace Antmicro.Migrant
         static Helpers()
         {
             TypesWriteableByPrimitiveWriter = new HashSet<Type>(typeof(PrimitiveWriter).GetMethods().Where(x => x.Name == "Write").Select(x => x.GetParameters()[0].ParameterType));
-            TypesWriteableByPrimitiveWriterAQNs = new HashSet<string>(TypesWriteableByPrimitiveWriter.Select(x => x.AssemblyQualifiedName));
             TypesWriteableByPrimitiveWriter.TrimExcess();
-            TypesWriteableByPrimitiveWriterAQNs.TrimExcess();
         }
 
         private static readonly HashSet<Type> TypesWriteableByPrimitiveWriter;
-        // this collection is here only to speed up lookups of `TypeDescriptor` objects
-        private static readonly HashSet<string> TypesWriteableByPrimitiveWriterAQNs;
         private static readonly int[] PaddingBoundaries = {
             128,
             1024,
