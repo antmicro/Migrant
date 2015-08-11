602768a0e76e9f453334b94f09b76aa6d9a31dfb
Mateusz Hołenko <mholenko@antmicro.com>
2015-08-11 13:04:53 +0200

    Comments added to version tolerance exception.

--- Migrant/VersionTolerance/VersionToleranceException.cs
+++ Migrant/VersionTolerance/VersionToleranceException.cs
@@ -27,27 +27,43 @@ using System.Runtime.Serialization;
 
 namespace Antmicro.Migrant.VersionTolerance
 {
+    /// <summary>
+    /// Class representing exception thrown when version tolerance verification mechanism detects disallowed changes in type structures.
+    /// </summary>
     public class VersionToleranceException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Antmicro.Migrant.VersionTolerance.VersionToleranceException"/> class.
+        /// </summary>
         public VersionToleranceException()
         {
         }
-        
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Antmicro.Migrant.VersionTolerance.VersionToleranceException"/> class.
+        /// </summary>
+        /// <param name="message">Message.</param>
         public VersionToleranceException(string message) : base(message)
         {
         }
-        
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Antmicro.Migrant.VersionTolerance.VersionToleranceException"/> class.
+        /// </summary>
+        /// <param name="info">Info.</param>
+        /// <param name="context">Context.</param>
         public VersionToleranceException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
-        
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Antmicro.Migrant.VersionTolerance.VersionToleranceException"/> class.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="innerException">Inner exception.</param>
         public VersionToleranceException(string message, Exception innerException) : base(message, innerException)
         {
         }
-        
     }
 }
 
